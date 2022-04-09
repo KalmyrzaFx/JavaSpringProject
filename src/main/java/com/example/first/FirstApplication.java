109b4d91410245c354eda4e62ace9c3dadbf2bf2
Kalmyrza <Kalmyrzastudy@gmail.com>
2022-04-09 23:20:03 +0600

    project

---
+++ src/main/java/com/example/first/FirstApplication.java
@@ -0,0 +1,17 @@
+package com.example.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+
+public class FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApplication.class, args);
+	}
+}

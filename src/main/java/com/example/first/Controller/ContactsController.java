44790743c09aa2ec34343565e67557a45d5cbdc0
Kalmyrza <Kalmyrzastudy@gmail.com>
2022-04-11 23:30:17 +0600

    adding html

--- src/main/java/com/example/first/Controller/ContactsController.java
+++ src/main/java/com/example/first/Controller/ContactsController.java
@@ -2,12 +2,17 @@ package com.example.first.Controller;
 
 import com.example.first.Models.ContactsDb;
 import com.example.first.repos.ContactsDbRepo;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class ContactsController {
@@ -32,6 +37,39 @@ public class ContactsController {
     public String postaddcontact(@RequestParam String name, @RequestParam String last_name,@RequestParam String email, @RequestParam Integer number, Model model) {
         ContactsDb contactsDb = new ContactsDb(name, last_name, email, number);
         contactsDbRepo.save(contactsDb);
+        return "redirect:/MyContacts";
+    }
+
+    @GetMapping("/MyContacts/{id}/edit")
+    public String contactEdit(@PathVariable(value = "id") long id, Model model) {
+        if(!contactsDbRepo.existsById(id)) {
+            return "redirect:/MyContacts";
+        }
+
+        Optional<ContactsDb> contactsDb = contactsDbRepo.findById(id);
+        ArrayList<ContactsDb> result = new ArrayList<>();
+        contactsDb.ifPresent(result::add);
+        model.addAttribute("ContactDb", result);
+        return "contact-edit";
+    }
+
+    @PostMapping("/MyContacts/{id}/edit")
+    public String postupdatecontact(@PathVariable(value = "id") long id, @RequestParam String name, @RequestParam String last_name,@RequestParam String email, @RequestParam Integer number, Model model) {
+
+        ContactsDb contactsDb = contactsDbRepo.findById(id).orElseThrow(IllegalStateException::new);
+        contactsDb.setName(name);
+        contactsDb.setLast_name(last_name);
+        contactsDb.setEmail(email);
+        contactsDb.setNumber(number);
+        contactsDbRepo.save(contactsDb);
+        return "redirect:/MyContacts";
+    }
+
+    @PostMapping("/MyContacts/{id}/delete")
+    public String postdeletecontact(@PathVariable(value = "id") long id, Model model) {
+
+        ContactsDb contactsDb = contactsDbRepo.findById(id).orElseThrow(IllegalStateException::new);
+        contactsDbRepo.delete(contactsDb);
         return "redirect:/MyContacts";
     }
 }
